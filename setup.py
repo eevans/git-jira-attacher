ce787c4f2c154c9fe5ff8351c06620b4b21b1488
Michael Greene <michael.greene@gmail.com>
2009-07-28 15:11:22 -0500

    Add Trove classifiers and author/maintainer info for PyPI

--- setup.py
+++ setup.py
@@ -5,13 +5,24 @@ try:
 except:
     from distutils.core import setup
 
+
 setup(name='git-jira-attacher',
       version='0.1',
       description='Utility for integrating Git and JIRA workflows',
       long_description=open('README.rst').read(),
-      author='David Reiss',
       url='http://github.com/dreiss/git-jira-attacher/tree',
+      author='David Reiss',
+      maintainer='Michael Greene',
+      maintainer_email='michael.greene@gmail.com',
       license='MIT License',
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Topic :: Software Development :: Bug Tracking",
+        "Topic :: Software Development :: Version Control",
+      ],
       scripts=['jira-am', 'jira-apply'],
       py_modules=['common', 'git-jira-attacher'],
       install_requires=['suds >= 0.3.6'],
